e13720eee8d5360623eea4a44516439ddf0de907
mjje27 <140602252+mjje27@users.noreply.github.com>
2023-08-02 00:24:10 +0900

    Add files via upload
    지각생 업로드합니다ㅠㅠ

---
+++ BFS/2606-바이러스/p2606_MJ.java
@@ -0,0 +1,58 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+// 바이러스
+public class p2606_MJ {
+	
+	static ArrayList<Integer>[] array;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int comNum = Integer.parseInt(br.readLine());
+		int lineNum = Integer.parseInt(br.readLine());
+		
+		visited = new boolean[comNum+1];
+		
+		// 인접리스트 초기화
+		array = new ArrayList[comNum+1];
+		for(int i=1; i<comNum+1; i++) {
+			array[i] = new ArrayList<Integer>();
+		}
+		
+		StringTokenizer st;
+		for(int i=0; i<lineNum; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			array[start].add(end);
+			array[end].add(start);
+		}
+		
+		dfs(1);
+		
+		int ans = 0; // 바이러스에 걸린 컴퓨터 수
+		for(int i=2; i<comNum+1; i++) {
+			if(visited[i]==true)
+				ans++;
+		}
+		
+		System.out.println(ans);
+		
+		br.close();
+	}
+	
+	static void dfs(int num) {
+		visited[num] = true;
+		for(int i : array[num]) {
+			if(!visited[i])
+				dfs(i);
+		}
+	}
+}
