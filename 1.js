69e82bf71a6dd6e2780762940a1aa74385dfe17e
h-spear <80024307+h-spear@users.noreply.github.com>
2023-08-10 21:22:13 +0900

    update README.md

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const output_file = 'readme.md';
 const except_list = ['out', '.idea'];
 
-function check_condition(p) {
+function checkCondition(p) {
     if (!p.isDirectory()) {
         return false;
     }
@@ -17,14 +17,22 @@ function check_condition(p) {
 }
 
 const result = {};
+const studentMap = { HC: 0, YK: 1, MJ: 2, HJ: 3 };
 
 fs.readdirSync('./', { withFileTypes: true }).forEach((p) => {
     const name = p.name;
     const path = './' + name;
-    if (!check_condition(p)) {
+    if (!checkCondition(p)) {
         return;
     }
-    result[name] = fs.readdirSync(path).length;
+    const student = [0, 0, 0, 0];
+    result[name] = student;
+    fs.readdirSync(path).forEach((problemDir) => {
+        fs.readdirSync(path + '/' + problemDir).forEach((file) => {
+            const ini = file.substring(file.length - 7, file.length - 5);
+            ++student[studentMap[ini]];
+        });
+    });
 });
 
 // write md file
@@ -34,19 +42,42 @@ if (!fs.existsSync(output_file)) {
 fs.writeFileSync(output_file, '');
 
 // // 백준
-let total = 0;
+const total = [0, 0, 0, 0];
 fs.appendFileSync(
     output_file,
-    '## Algorithm\n|    Algorithm    | solved |\n| :-------------: | :----: |\n',
+    '## Algorithm\n|    Algorithm    | 김현창 | 양유경 | 정민지 | 정현주 |\n| :-------------: | :----: | :----: | :----: | :----: |\n',
     'utf-8'
 );
 
-for (let key in result) {
-    total += result[key];
-    temp = '|' + key + '|' + result[key] + '|\n';
+for (let alg in result) {
+    for (let i = 0; i < 4; ++i) total[i] += result[alg][i];
+    temp =
+        '|' +
+        alg +
+        '|' +
+        result[alg][0] +
+        '|' +
+        result[alg][1] +
+        '|' +
+        result[alg][2] +
+        '|' +
+        result[alg][3] +
+        '|\n';
     fs.appendFileSync(output_file, temp, 'utf-8');
 }
-fs.appendFileSync(output_file, '| **sum** | **' + total + '**|\n\n', 'utf-8');
-console.log('solved ' + total + '!');
+fs.appendFileSync(
+    output_file,
+    '| **합계** | **' +
+        total[0] +
+        '**|**' +
+        total[1] +
+        '**|**' +
+        total[2] +
+        '**|**' +
+        total[3] +
+        '**|\n\n',
+    'utf-8'
+);
 
+console.log('solved ' + total + '!');
 console.log('saved successfully! ' + output_file);
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## Algorithm
-|    Algorithm    | solved |
-| :-------------: | :----: |
-|BackTracking|3|
-|BFS|8|
-|Bruteforce|4|
-|Implematation|2|
-|TwoPointer|1|
-| **sum** | **18**|
+|    Algorithm    | 김현창 | 양유경 | 정민지 | 정현주 |
+| :-------------: | :----: | :----: | :----: | :----: |
+|BackTracking|3|1|1|1|
+|BFS|8|6|5|8|
+|Bruteforce|4|4|2|4|
+|Implematation|2|0|0|1|
+|TwoPointer|1|1|1|1|
+| **합계** | **18**|**12**|**9**|**15**|
 
