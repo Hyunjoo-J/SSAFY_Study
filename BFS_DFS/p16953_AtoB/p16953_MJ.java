9c96dd5b3312c8cb8cc4fd2811871513b80ac758
mjje27 <140602252+mjje27@users.noreply.github.com>
2023-08-16 19:15:35 +0900

    p16953 정민지 (#24)

---
+++ BFS_DFS/p16953_AtoB/p16953_MJ.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	static long A, B;
+	static int cnt;
+	static Queue<Long> queue = new LinkedList<>();
+	static boolean visited[];
+
+	static void bfs() {
+		while (!queue.isEmpty()) {
+			int queueSize = queue.size();
+			cnt++;
+			for (int i = 0; i < queueSize; i++) {
+				long num = queue.poll();
+
+				if (num == B) {
+					return;
+				}
+				
+				if (num * 2 <= B && visited[(int)(num * 2)] == false) {
+					queue.offer(num * 2);
+					visited[(int)(num * 2)] = true;
+				}
+
+				if (num * 10 + 1 <= B && visited[(int)(num * 10 + 1)] == false) {
+					queue.offer((num * 10) + 1);
+					visited[(int)(num * 10 + 1)] = true;
+				}
+			}
+		}
+		cnt = -1;
+		return;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		visited = new boolean[1000000000];
+
+		queue.offer(A);
+		visited[(int)(A)] = true;
+		
+		bfs();
+
+		System.out.println(cnt);
+		
+		sc.close();
+	}
+}
