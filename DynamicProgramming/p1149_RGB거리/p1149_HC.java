c60a51f945ed1d8b38064404c1f94591b334e404
h-spear <80024307+h-spear@users.noreply.github.com>
2023-08-20 14:02:59 +0900

    p1149 김현창 (#36)

---
+++ DynamicProgramming/p1149_RGB거리/p1149_HC.java
@@ -0,0 +1,35 @@
+package DynamicProgramming.p1149_RGB거리;
+
+import java.io.*;
+import java.util.*;
+
+public class p1149_HC {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] cost = new int[N][3];
+        int[][] dp = new int[N][3];
+
+        for (int i = 0; i < N; ++i) {
+            st = new StringTokenizer(br.readLine());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+        for (int i = 1; i < N; ++i) {
+            dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        int answer = Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2]));
+        System.out.println(answer);
+    }
+}
