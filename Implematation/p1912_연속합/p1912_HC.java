fbc371312a841ecf0f5d072e89c3dcc63fb48cb1
h-spear <80024307+h-spear@users.noreply.github.com>
2023-08-27 01:34:51 +0900

    p1912 김현창 (#39)

---
+++ Implematation/p1912_연속합/p1912_HC.java
@@ -0,0 +1,29 @@
+package Implematation.p1912_연속합;
+
+import java.io.*;
+import java.util.*;
+
+public class p1912_HC {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int answer = dp[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
