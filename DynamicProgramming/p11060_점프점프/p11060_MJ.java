a4b31d3718850bb08cbbf54302824e3b59770b10
Youkyung Yang <gosjorb@gmail.com>
2023-08-26 20:11:15 +0900

    rename files

--- DynamicProgramming/p11060_점프점프/p11060_MJ.java
+++ DynamicProgramming/p11060_점프점프/p11060_MJ.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class p11060_MJ {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int N, map[];
