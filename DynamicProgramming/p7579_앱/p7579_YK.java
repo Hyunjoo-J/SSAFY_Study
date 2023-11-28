dfdad3f8dffdf2ed442d7b103f16c02605ae3c2a
Youkyung <gosjorb@gmail.com>
2023-11-28 14:50:27 +0900

    p7579 양유경 재제출!

--- DynamicProgramming/p7579_앱/p7579_YK.java
+++ DynamicProgramming/p7579_앱/p7579_YK.java
@@ -35,18 +35,16 @@ public class p7579_YK {
 
         dp = new int[N + 1][costs + 1];
         for (int i = 1; i <= N; ++i) {
-            for (int j = 1; j <= costs; ++j) {
+            for (int j = 0; j <= costs; ++j) {
                 int m = mm[i];
                 int c = cost[i];
                 if (c <= j)
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - c] + m);
-
                 dp[i][j] = Math.max(dp[i - 1][j], dp[i][j]);
-
             }
         }
 
-        for (int i = 1; i <= costs; i++) {
+        for (int i = 0; i <= costs; i++) {
             if (dp[N][i] >= M) {
                 bw.write(String.valueOf(i));
                 break;
